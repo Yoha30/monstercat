ddd7acc7034712c2755468a66b466014183c0eb9
Yoha30 <3202114888@alexu.edu.eg>
2021-12-18 04:54:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'main':"url('images/R-15351962-1590167974-4553.jpeg.jpg')",
+      },
+      backgroundColor:{
+        'green':'#50A584',
+      },
+      brightness:{
+        25:'0.35',
+      },
+      backdropBlur:{
+        md:'6px',
+      },
+      zIndex:{
+        1:'-1',
+      },
+      width:{
+        43:'43%',
+        87:'87%',
+      },
+      colors:{
+        'light-green' :'#50A584',
+      },
+      inset:{
+        42:'10.5rem',
+      },
+    },
+    },
+  plugins: [],
+}
