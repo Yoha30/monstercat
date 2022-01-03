7a2663166706b7d98fb825a3623dbaf9381b6865
Yoha30 <3202114888@alexu.edu.eg>
2022-01-04 01:19:07 +0200

    ..

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const playButton = document.querySelectorAll('.play-button');
 const pause = document.querySelectorAll('.pause-button');
 const playList = document.querySelector('#play-list');
+const listenNow = document.querySelector('#listen');
+let clickCounter = 0;
+function playlisten(e) {
+    let id = e.currentTarget.id;
+    const Listen = listenNow.querySelector(`[aria-labelledby = '${id}']`);
+    Listen.play();
+    clickCounter++;
+    if (clickCounter % 2 === 0) {
+        Listen.pause();
+    }
 
+}
 
 playButton.forEach(button => button.addEventListener('click', function (e) {
-    const id = e.currentTarget.id;
+    let id = e.currentTarget.id;
     const start = playList.querySelector(`[aria-labelledby = '${id}']`);
     const songs = playList.querySelector(`[role='${id}']`);
-    if (!button.classList.add('hidden')) {
+    if (id = start) {
         songs.classList.remove('hidden');
         button.classList.add('hidden');
         start.play();
-        const audioContext = new AudioContext();
-        const element = document.querySelectorAll('audio');
-
     }
     songs.addEventListener('click', function () {
-        start.pause();
         songs.classList.add('hidden');
         button.classList.remove('hidden');
+        start.pause();
+
     });
 }));
+
 
